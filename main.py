421fd3ad39f528eb6198b641ce18a6145870e696
Maksym Diachuk <dyachukmv@gmail.com>
2024-02-10 11:57:19 +0200

    init repo with joke app from lecture

---
+++ main.py
@@ -0,0 +1,89 @@
+import datetime as dt
+import json
+
+import requests
+from flask import Flask, jsonify, request
+
+# create your API token, and set it up in Postman collection as part of the Body section
+API_TOKEN = ""
+# you can get API keys for free here - https://api-ninjas.com/api/jokes
+RSA_KEY = ""
+
+app = Flask(__name__)
+
+
+class InvalidUsage(Exception):
+    status_code = 400
+
+    def __init__(self, message, status_code=None, payload=None):
+        Exception.__init__(self)
+        self.message = message
+        if status_code is not None:
+            self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        rv = dict(self.payload or ())
+        rv["message"] = self.message
+        return rv
+
+
+def generate_joke(exclude: str, limit: int = 1):
+    url_base_url = "https://api.api-ninjas.com"
+    limit = 1
+    url_api = "jokes"
+    url_api_version = "v1"
+
+    url = f"{url_base_url}/{url_api_version}/{url_api}?limit={limit}"
+
+    headers = {"X-Api-Key": RSA_KEY}
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == requests.codes.ok:
+        return json.loads(response.text)
+    else:
+        raise InvalidUsage(response.text, status_code=response.status_code)
+
+
+@app.errorhandler(InvalidUsage)
+def handle_invalid_usage(error):
+    response = jsonify(error.to_dict())
+    response.status_code = error.status_code
+    return response
+
+
+@app.route("/")
+def home_page():
+    return "<p><h2>KMA L2: python Saas.</h2></p>"
+
+
+@app.route("/content/api/v1/integration/generate", methods=["POST"])
+def joke_endpoint():
+    start_dt = dt.datetime.now()
+    json_data = request.get_json()
+
+    if json_data.get("token") is None:
+        raise InvalidUsage("token is required", status_code=400)
+
+    token = json_data.get("token")
+
+    if token != API_TOKEN:
+        raise InvalidUsage("wrong API token", status_code=403)
+
+    exclude = ""
+    if json_data.get("exclude"):
+        exclude = json_data.get("exclude")
+
+    joke = generate_joke(exclude)
+
+    end_dt = dt.datetime.now()
+
+    result = {
+        "event_start_datetime": start_dt.isoformat(),
+        "event_finished_datetime": end_dt.isoformat(),
+        "event_duration": str(end_dt - start_dt),
+        "joke": joke,
+    }
+
+    return result
